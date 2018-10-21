c537fe20fa46946d8cb39dbbcdbdd8b9c8663b68
Your Name <you@example.com>
2018-10-21 11:06:48 -0300

    plots animados

--- examples/Plot-Gif-v0.1.jl
+++ examples/Plot-Gif-v0.1.jl
@@ -1,16 +1,25 @@
 using Plots
 
-
-# initialize a 3D plot with 1 empty series
-plt = plot3d(1, xlim=(-50,50), ylim=(-50,500), zlim=(-2500,2500),
-                title = "Paraboloide Hiperbólico", marker = 1)
-
 function parabol_hiperbol!(x::Int64, y::Int64)
     x^2 - y^2
 end
-parabol_hiperbol!(2,1)
 
+function parabol_elip!(x::Int64, y::Int64)
+    x^2 + y^2
+end
+
+# initialize a 3D plot with 1 empty series
+ph = plot3d(1, xlim=(-50,50), ylim=(-50,500), zlim=(-2500,2500),
+                title = "Paraboloide Hiperbólico", marker = 1)
 @gif for x = -50:50, y = -50:50
     z = parabol_hiperbol!(x,y)
-    push!(plt, float(x), float(y), float(z))
+    push!(ph, float(x), float(y), float(z))
+end every 100
+
+
+pe = plot3d(1, xlim=(-50,50), ylim=(-50,500), zlim=(-2500,2500),
+                title = "Paraboloide Elíptico", marker = 1)
+@gif for x = -50:50, y = -50:50
+    z = parabol_elip!(x,y)
+    push!(pe, float(x), float(y), float(z))
 end every 100
